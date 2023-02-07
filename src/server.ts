bef7defae62b67564cc32c89a9872aad849736ac
Raphael Berger <raphaelmb@gmail.com>
2023-02-07 12:35:20 -0300

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import fastify from 'fastify'
+import { knex } from './database'
+import { env } from './env'
+
+const app = fastify()
+
+app.get('/hello', async () => {
+  const transactions = await knex('transactions').select('*')
+  return transactions
+})
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTTP Server is running...')
+  })
